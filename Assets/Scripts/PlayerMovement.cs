607cdbbcd665f01fdd20a8a506709948cf9cee67
Rodrigo Peroba de Souza <rodperoba@hotmail.com>
2025-10-17 10:58:37 -0300

    Conserto da queda
    -Adicionei um código que aumenta a velocidade do player para baixo quando ele não encosta no chão
    -Retirei a gravidade do Rigidbody
    -Ajustei o collider da escada que tava impedindo de subir

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
+using System;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -13,13 +14,25 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 velocidade;
     private Vector2 cameraVelocity;
     private Vector3 targetAngle;
-    
+
+    //Queda
+    [SerializeField] float fallVelocity = 10f;
+    [SerializeField] float fallVelocityMultiplier = 1.05f;
+    [SerializeField] float fallVelocityIncrement = 0.05f;
+    private float currentFallVelocity;
+    private bool isGrounded = true;
+    private float raycastDistance;
+    private float playerHeight;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
         targetAngle = this.gameObject.transform.rotation.eulerAngles;
+        //TODO Mudar isso quando trocar o collider
+        playerHeight = this.GetComponent<CapsuleCollider>().height;
+        raycastDistance = playerHeight + 0.15f;
+        currentFallVelocity = fallVelocityMultiplier;
     }
 
     private void FixedUpdate()
@@ -28,6 +41,19 @@ public class PlayerMovement : MonoBehaviour
         targetAngle += new Vector3(cameraVelocity.y * cameraspeed.y, cameraVelocity.x * cameraspeed.x, 0) * Time.fixedDeltaTime * 300;
         rb.MoveRotation(Quaternion.Euler(0,targetAngle.y,0));
         m_camera.transform.rotation = Quaternion.Euler(targetAngle);
+        //Talvez castar esse raycast todo FixedUpdate seja pesado, testar depois
+        checkIsGrounded();
+        if (!isGrounded)
+        {
+            rb.velocity += Vector3.down * fallVelocity * currentFallVelocity * Time.fixedDeltaTime;
+            currentFallVelocity += fallVelocityIncrement;
+        }
+        else
+        {
+            currentFallVelocity = fallVelocityMultiplier;
+        }
+        Debug.Log(isGrounded);
+        Debug.DrawRay(transform.position, Vector3.down * raycastDistance, Color.red);
     }
 
     public void Move(InputAction.CallbackContext value)
@@ -38,5 +64,18 @@ public class PlayerMovement : MonoBehaviour
     public void MoveCamera(InputAction.CallbackContext value)
     {
         cameraVelocity = value.ReadValue<Vector2>();
+    }
+
+    private void checkIsGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
     }
 }
